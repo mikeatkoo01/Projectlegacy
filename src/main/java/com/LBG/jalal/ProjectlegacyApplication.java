23ea466becc70e1846e2c974100b8181a6d5acd4
unknown <mikeatkoo01@gmail.com>
2024-02-26 13:54:15 +0000

    ma frist comm

---
+++ src/main/java/com/LBG/jalal/ProjectlegacyApplication.java
@@ -0,0 +1,13 @@
+package com.LBG.jalal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectlegacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LBG/jalal/ProjectlegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LBG.jalal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
