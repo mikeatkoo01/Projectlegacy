de9900f01592fa1760d86d8636d17336789d0ed9
unknown <mikeatkoo01@gmail.com>
2024-02-29 12:00:00 +0000

    hello

--- src/main/java/com/LBG/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.LBG.repo;
-
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
